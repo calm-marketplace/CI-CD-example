2a7f3802b4f32d995aca9ef12c010a49cc38c2ac
Thomas Findelkind <webmaster@thomas-findelkind.de>
2018-10-11 18:19:15 +0200

    rm afs file

--- repos/prepare_repos.sh
+++ repos/prepare_repos.sh
@@ -11,7 +11,7 @@
 #reposync -l --repoid=mongodb-org-3.4   --download_path=. --downloadcomps --download-metadata
 
 ### Docker-CE 
-reposync -l --repoid=docker --download_path=. --downloadcomps --download-metadata
+#reposync -l --repoid=docker-ce-stable --download_path=. --downloadcomps --download-metadata
 
 
 #createrepo ./base/ -g comps.xml
@@ -21,3 +21,5 @@ reposync -l --repoid=docker --download_path=. --downloadcomps --download-metadat
 #createrepo ./epel/
 
 #createrepo ./mongodb-org-3.4/RPMS
+
+createrepo ./docker-ce-stable/Packages
