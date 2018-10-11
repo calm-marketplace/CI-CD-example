48ab03081012a866e33cff7c41ac886b01ce4467
Thomas Findelkind <webmaster@thomas-findelkind.de>
2018-10-11 12:51:19 +0200

    new start

---
+++ images/prepare_images.sh
@@ -0,0 +1,1 @@
+wget -O CentOS-7-x86_64-GenericCloud.qcow2 https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud-1809.qcow2
---
+++ repos/prepare_repos.sh
@@ -0,0 +1,23 @@
+### Centos 7 
+#reposync --gpgcheck -l --repoid=base       --download_path=. --downloadcomps --download-metadata
+#reposync --gpgcheck -l --repoid=centosplus --download_path=. --downloadcomps --download-metadata
+#reposync --gpgcheck -l --repoid=extras     --download_path=. --downloadcomps --download-metadata
+#reposync --gpgcheck -l --repoid=updates    --download_path=. --downloadcomps --download-metadata
+
+### EPEL 
+#reposync -l --repoid=epel --download_path=. --downloadcomps --download-metadata
+
+### MongoDB 3.4 for Centos 7
+#reposync -l --repoid=mongodb-org-3.4   --download_path=. --downloadcomps --download-metadata
+
+### Docker-CE 
+reposync -l --repoid=docker --download_path=. --downloadcomps --download-metadata
+
+
+#createrepo ./base/ -g comps.xml
+#createrepo ./centosplus/
+#createrepo ./extras/
+#createrepo ./updates/
+#createrepo ./epel/
+
+#createrepo ./mongodb-org-3.4/RPMS
