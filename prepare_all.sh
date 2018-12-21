d42157166a39e633338770b12b8c3200ecaaeb0e
Thomas Findelkind <webmaster@thomas-findelkind.de>
2018-12-21 09:50:14 +0100

    prepare_all2

--- prepare_all.sh
+++ prepare_all.sh
@@ -1,14 +1,12 @@
-#mkdir bootstrap
-#cd bootstrap
-#mkdir repos
-#cd repos
-#../../repos/prepare_repos.sh
-##cd ..
-#mkdir downloads
-#cd downloads
-#../../downloads/prepare_downloads.sh
-#cd ..
-#cd ..
-#cp -R keys ./bootstrap/
-mkisofs -o bootstrap.iso ./bootstrap
-
+mkdir bootstrap
+cd bootstrap
+mkdir repos
+cd repos
+../../repos/prepare_repos.sh
+cd ..
+mkdir downloads
+cd downloads
+../../downloads/prepare_downloads.sh
+cd ..
+cd ..
+cp -R keys ./bootstrap/
